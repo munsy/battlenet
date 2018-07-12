c80b4df49cdf41e2c62b85c6d7016f78d57f0456
tmunson <tmunson@wsu.edu>
2018-07-11 17:38:27 -0700

    Add Get() method to BNetClient

--- client.go
+++ client.go
@@ -1,8 +1,10 @@
 package battlenet
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -17,6 +19,12 @@ type BNetClient struct {
 	userAgent string
 	client    *http.Client
 	locale    locale.Locale
+}
+
+// BNetSettings defines settings for a BNetClient.
+type BNetSettings struct {
+	Client *http.Client
+	Locale locale.Locale
 }
 
 // New creates a new BNetClient. Passing different interface types can cause
@@ -40,13 +48,41 @@ func New(args ...interface{}) (c *BNetClient, err error) {
 		case locale.Locale:
 			c.locale = t
 			return c, nil
-		case string:
-			return c, nil
-		case []string:
+		case BNetSettings:
+			c.client = t.Client
+			c.locale = t.Locale
 			return c, nil
 		default:
 			return nil, errors.New(fmt.Sprintf("Type %v is not supported.", t))
 		}
 	}
 	panic(errors.New(fmt.Sprintf("Unresolved arguments...")))
+}
+
+// Convert an HTTP response from a given URL to the supplied interface.
+// This function expects the body to contain the associated JSON response
+// from the given URL and will return an error if it fails to properly unmarshal.
+func (c *BNetClient) Get(url string, v interface{}) error {
+	if nil == v {
+		return errors.New("No interface supplied.")
+	}
+
+	response, err := http.Get(url)
+	if nil != err {
+		return err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if nil != err {
+		return err
+	}
+
+	err = json.Unmarshal([]byte(body), &v)
+	if nil != err {
+		return err
+	}
+
+	return nil
 }
