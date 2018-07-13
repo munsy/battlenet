6c0328e7bf1d7e92a3157f34d7fa9bad8f67b0c1
tmunson <tmunson@wsu.edu>
2018-07-12 20:22:41 -0700

    Update ladder.go

--- sc2/ladder.go
+++ sc2/ladder.go
@@ -1,1 +1,16 @@
 package sc2
+
+type LadderMember struct {
+	Character      Character `json:"character"`
+	JoinTimestamp  int       `json:"joinTimestamp"`
+	Points         int       `json:"points"`
+	Wins           int       `json:"wins"`
+	Losses         int       `json:"losses"`
+	HighestRank    int       `json:"highestRank"`
+	PreviousRank   int       `json:"previousRank"`
+	FavoriteRaceP1 string    `json:"favoriteRaceP1"`
+}
+
+type Ladder struct {
+	LadderMembers []LadderMember `json:"ladderMembers"`
+}
