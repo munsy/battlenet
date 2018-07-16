bd85a630be7c2dc2348df16aadadfcb04f47134a
tmunson <tmunson@wsu.edu>
2018-07-15 18:06:05 -0700

    fix sc2 endoints

--- sc2/endpoints.go
+++ sc2/endpoints.go
@@ -3,32 +3,32 @@ package sc2
 import (
 	"strconv"
 
-	"github.com/munsy/gobattlenet/regions"
+	"github.com/munsy/gobattlenet"
 )
 
 var (
 	// SC2 API
-	sc2 = regions.API() + "sc2/"
+	sc2 = func(r battlenet.Region) string { return r.API() + "sc2/" }
 	// PROFILE
-	endpointProfile = sc2 + "profile/"
-	user            = endpointProfile + "user"                                              // SC2 OAUTH PROFILE 	/SC2/PROFILE/USER
-	profile         = func(r battlenet.Region, profileID int, region, name string) string { // PROFILE				/SC2/PROFILE/:ID/:REGION/:NAME/
-		return endpointProfile + strconv.Itoa(profileID) + "/" + region + "/" + name + "/"
+	endpointProfile = func(r battlenet.Region) string { return sc2(r) + "profile/" }
+	user            = func(r battlenet.Region) string { return endpointProfile(r) + "user" } // SC2 OAUTH PROFILE 	/SC2/PROFILE/USER
+	profile         = func(r battlenet.Region, profileID int, region, name string) string {  // PROFILE				/SC2/PROFILE/:ID/:REGION/:NAME/
+		return endpointProfile(r) + strconv.Itoa(profileID) + "/" + region + "/" + name + "/"
 	}
-	ladderProfile = func(profileID int, region, name string) string { // LADDERS 	/SC2/PROFILE/:ID/:REGION/:NAME/LADDERS
-		return profile(profileID, region, name) + "ladders"
+	ladderProfile = func(r battlenet.Region, profileID int, region, name string) string { // LADDERS 	/SC2/PROFILE/:ID/:REGION/:NAME/LADDERS
+		return profile(r, profileID, region, name) + "ladders"
 	}
 
-	matchHistory = func(profileID int, region, name string) string { // MATCH HISTORY 	/SC2/PROFILE/:ID/:REGION/:NAME/MATCHES
-		return profile(profileID, region, name) + "matches"
+	matchHistory = func(r battlenet.Region, profileID int, region, name string) string { // MATCH HISTORY 	/SC2/PROFILE/:ID/:REGION/:NAME/MATCHES
+		return profile(r, profileID, region, name) + "matches"
 	}
 
 	// LADDER
-	endpointLadder = sc2 + "ladder/"
-	ladder         = func(id int) string { return endpointLadder + strconv.Itoa(id) } // LADDER /SC2/LADDER/:ID
+	endpointLadder = func(r battlenet.Region) string { return sc2(r) + "ladder/" }
+	ladder         = func(r battlenet.Region, id int) string { return endpointLadder(r) + strconv.Itoa(id) } // LADDER /SC2/LADDER/:ID
 
 	// DATA RESOURCES
-	endpointData = sc2 + "data/"
-	achievements = endpointData + "achievements" // ACHIEVEMENTS 	/SC2/DATA/ACHIEVEMENTS
-	rewards      = endpointData + "rewards"      // REWARDS 		/SC2/DATA/REWARDS
+	endpointData = func(r battlenet.Region) string { return sc2(r) + "data/" }
+	achievements = func(r battlenet.Region) string { return endpointData(r) + "achievements" } // ACHIEVEMENTS 	/SC2/DATA/ACHIEVEMENTS
+	rewards      = func(r battlenet.Region) string { return endpointData(r) + "rewards" }      // REWARDS 		/SC2/DATA/REWARDS
 )
