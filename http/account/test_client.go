e770446a7047794c564af7d445fce5892e2d3e96
tmunson <tmunson@wsu.edu>
2018-07-27 00:11:55 -0700

    more client tests

---
+++ http/account/test_client.go
@@ -0,0 +1,24 @@
+package account
+
+import (
+	"testing"
+
+	"github.com/munsy/battlenet/locale"
+	"github.com/munsy/battlenet/regions"
+)
+
+func TestNew(t *testing.T) {
+	c, err := New(nil, regions.US, locale.AmericanEnglish, "no-token", "test")
+
+	if nil != err {
+		t.Fatal(err.Error())
+	}
+
+	if nil == c.client {
+		t.Fatal("nil client")
+	}
+
+	if c.UserAgent() != "GoBattleNet/test" {
+		t.Fatal("wrong version")
+	}
+}
---
+++ http/sc2/client_test.go
@@ -0,0 +1,24 @@
+package sc2
+
+import (
+	"testing"
+
+	"github.com/munsy/battlenet/locale"
+	"github.com/munsy/battlenet/regions"
+)
+
+func TestNew(t *testing.T) {
+	c, err := New(nil, regions.US, locale.AmericanEnglish, "no-key", "test")
+
+	if nil != err {
+		t.Fatal(err.Error())
+	}
+
+	if nil == c.client {
+		t.Fatal("nil client")
+	}
+
+	if c.UserAgent() != "GoBattleNet/test" {
+		t.Fatal("wrong version")
+	}
+}
